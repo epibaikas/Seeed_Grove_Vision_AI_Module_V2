f44ba5be940a1b8db74cf39b1edb5494a5d18c29
Fanis Baikas <fan.baikas@gmail.com>
2024-12-09 12:17:49 +0000

    Updated .gitignore and flash.sh, untracked EPII_CM55M_APP/prebuilt_libs/ and we2_image_gen_local/secureboot_tool/

--- EPII_CM55M_APP_S/flash.sh
+++ EPII_CM55M_APP_S/flash.sh
@@ -2,5 +2,5 @@
 
 echo "Flash device using python script"
 
-SERIAL_PORT="/dev/cu.usbmodem578D0263771"
+SERIAL_PORT="/dev/cu.usbmodem58C60539331"
 python ../xmodem/xmodem_send.py --port=$SERIAL_PORT --baudrate=921600 --protocol=xmodem --file=../we2_image_gen_local/output_case1_sec_wlcsp/output.img
